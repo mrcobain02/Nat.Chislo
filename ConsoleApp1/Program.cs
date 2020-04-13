e1ef1db1ac726b573339d6c4c2a197f05c854d25
Вадим Спирин <vadim.spirin.1990@bk.ru>
2020-04-13 19:22:27 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool av = true;
+            Console.WriteLine("Введите число");
+            double a = Convert.ToInt32(Console.ReadLine());
+            if (a / 0)
+            {
+                av = false;
+            }
+        }
+    }
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                bool prost = true;
+                Console.WriteLine("Введите число");
+                int n = Convert.ToInt32(Console.ReadLine());
+                for (int i = 2; i <= n / 2; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        prost = false;
+                        break;
+                    }
+                }
+                if (prost)
+                {
+                    Console.WriteLine("Число простое");
+                }
+                else
+                {
+                    Console.WriteLine("Число не простое");
+                }
+                Console.ReadKey();
+            
+
+        }
+    }
+}
